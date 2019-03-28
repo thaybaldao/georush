dbd7b56f9ae5191ad3596d28f3d61782827f9263
Raissa Pimentel <raissabmpimentel@gmail.com>
2019-03-28 20:02:13 -0300

    New obstacles draws

---
+++ Projeto/Obstacle.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.locals import *
+import os
+
+class Obstacle:
+    def __init__(self, x, y, width, height, img, type):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.img = img
+        self.type = type
+        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def draw(self, win):
+        self.updateHitbox()
+        pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+        win.blit(self.img, (self.x, self.y))
+
+    def updateHitbox(self):
+        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def identifyObstacleType(self):
+        return self.type;
+
+#    def collide(self, rect):
+#        if(self.hitbox.colliderect(rect))
+#            if(
