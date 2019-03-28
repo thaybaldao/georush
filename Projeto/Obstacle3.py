5d8522eba5b68f2506e0ac89515b1bc9252f09e4
thaybaldao <thayna.baldao@gmail.com>
2019-03-28 19:55:08 -0300

    trying to fix collisions method

--- Projeto/Obstacle3.py
+++ Projeto/Obstacle3.py
@@ -1,4 +1,5 @@
 import random
+from math import *
 
 class Obstacle3():
     def __init__(self, obj1, obj2, obj3, obj4, obj5):
@@ -10,7 +11,14 @@ class Obstacle3():
         self.x = obj2.x
         self.r1 = random.randrange(0,2)
         self.r2 = random.randrange(0,2)
-        self.width = 835
+
+        self.objects = []
+        self.objects.append(self.obj1)
+        self.objects.append(self.obj1)
+        self.objects.append(self.obj1)
+        self.objects.append(self.obj1)
+        self.objects.append(self.obj1)
+
 
     def draw(self, win):
         self.obj1.draw(win)
@@ -28,4 +36,21 @@ class Obstacle3():
             if obj.x >= -850:
                 obj.x -= 1.4
         self.x = self.obj2.x
+
+    def collisionStatus(self, rect):
+        for object in self.objects:
+            if object.hitbox.colliderect(rect):
+                if object.type == 'triangle':
+                    return 'death'
+                else:
+                    x = object.hitbox.centerx - rect.centerx
+                    y = rect.centerx - object.hitbox.centerx
+                    collisionAngle = atan2(y, x)
+                    maxLateralCollisionAngle = atan2(object.hitbox.height + rect.height,
+                                                     object.hitbox.width + rect.width)
+
+                    if collisionAngle > maxLateralCollisionAngle and collisionAngle < pi - maxLateralCollisionAngle:
+                        return 'continue'
+
+                    return 'death'
 
