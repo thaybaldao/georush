a60aaeb2983a596d92cde5fb6897dbd83e4fa616
thaybaldao <thayna.baldao@gmail.com>
2019-03-29 13:04:03 -0300

    adding settings file

---
+++ Projeto/Settings.py
@@ -0,0 +1,17 @@
+# game options/settings
+TITLE = "Geo Rush"
+WIDTH = 800
+HEIGHT = 500
+FPS = 50
+
+# Player properties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
